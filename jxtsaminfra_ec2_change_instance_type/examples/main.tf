727bb5fdcc19dec598e34e3594b40ce41473ea23
Samuel Frimpong <samuel.frimpong@hubtel.com>
2025-06-28 21:45:00 +0000

    fix goreleaser step on actions

--- jxtsaminfra_ec2_change_instance_type/examples/main.tf
+++ jxtsaminfra_ec2_change_instance_type/examples/main.tf
@@ -2,4 +2,5 @@ resource "jxtsaminfra_instance_type_changer" "main" {
     for_each = local.instances
   instance_id          = each.value.instance_id 
   target_instance_type = each.value.target_instance_type
+
 }
